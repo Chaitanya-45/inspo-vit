3e32071a8e4f18fe056fa4142c6c97ac9c25a59d
Chaitanya <139270341+Chaitanya-45@users.noreply.github.com>
2024-03-20 18:41:04 +0530

    Update cscript.js

--- cscript.js
+++ cscript.js
@@ -5,7 +5,7 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null;
-const API_KEY = "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"; // Paste your API key here
+const API_KEY = ""; // Paste your API key here
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
